42449949050cd8c44010b1a4bf90ad6bdb9be4c2
Daniil Rusanov <daniil12099@gmail.com>
2024-11-20 18:53:23 +0200

    20.11 19:00

--- elven_service/js/menu.js
+++ elven_service/js/menu.js
@@ -45,4 +45,17 @@ document.addEventListener("DOMContentLoaded", () => {
       firefliesContainer.appendChild(firefly);
     }
   });
+
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetElement = document.querySelector(this.getAttribute('href'));
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    });
+  });
   
